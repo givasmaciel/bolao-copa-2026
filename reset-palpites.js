19143f4304683ac4bb684b8932772ab5265773a0
Givanildo <gpmmac@gmail.com>
2026-07-04 14:51:21 -0300

    chore: remove root scripts moved to scripts/ directory

--- reset-palpites.js
+++
@@ -1,9 +0,0 @@
-// Resetar palpites e jogos finalizados para teste limpo
-const { run } = require('./database/db');
-
-(async () => {
-  await run('DELETE FROM palpites');
-  await run('UPDATE jogos SET gols_casa = NULL, gols_visitante = NULL, finalizado = 0');
-  console.log('✅ Palpites e resultados zerados');
-  process.exit(0);
-})();
